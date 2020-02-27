4c0e9f50dba4d99a2cf25c48bdd42ec860265e6a
EMC <edwardcoad.ec@gmail.com>
2020-02-27 19:46:01 +1030

    test

--- public/assets/js/dActive.js
+++ public/assets/js/dActive.js
@@ -1,7 +1,7 @@
 // onclick for deleting a donation
 $(document).on("click", ".delete-donation", handleDelete);
 
-// Function for handling what happens when the delete button is pressed
+// Function for handling what happens when the delete button is pressed.
 function handleDelete() {
     var id = $(this).parent().data("id");
     $.ajax({
